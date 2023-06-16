6176d6352f22112590f227f6a8a7842fa780fa2c
AdityaMundhe09 <adityamundhe6993@gmail.com>
2023-06-16 11:57:04 +0530

    new logic
    add new logic

--- LeetCodeDay1/src/main/TwoSum.java
+++ LeetCodeDay1/src/main/TwoSum.java
@@ -1,33 +1,47 @@
 package main;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class TwoSum {
 
 	public static int[] sum(int[] nums, int target) {
 		// TODO Auto-generated method stub
 		
-		Map<Integer,Integer> map = new HashMap<>();
+//		HashMap<Integer,Integer> valToindex = new HashMap<>();
+//
+//
+//		for(int i = 0 ; i < nums.length ; i++){
+//		if(valToindex.containsKey(target - nums[i])){
+//		return new int[] {valToindex.get(target - nums[i]), i};
+//		}
+//		valToindex.put(nums[i], i);
+//		}
+//		return nums;
 		
-		
-		for(int i=0;i<nums.length-1;i++) {
-			if(map.containsKey(target-nums[i])) {
-				return new int[] {map.get(target-nums[i]),i};
-			}
-			map.put(nums[i], i);
-		}
-	
-		
-		return nums;
+		int res[] = new int[2];
+        for (int i = 0; i < nums.length-1; i++)
+        {
+            for (int j = i+1; j < nums.length; j++)
+            {
+                if (nums[j] + nums[i] == target)
+                {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
 	}
 	
 	public static void main(String[] args) {
 		
-		int[] a = {2,7,11,15};
-		int target = 9;
+		int[] a = {3,2,3};
+		int target = 6;
 		
-		System.out.println("Output: "+sum(a,target));
+		
+		int[] b = sum(a,target);
+		System.out.println("Output: "+ b[0] + b[1]);
 	}
 
 	
